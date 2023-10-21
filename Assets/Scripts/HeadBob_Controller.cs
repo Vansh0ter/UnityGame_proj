62f8b5cfd3be9e4844b5ce08785d97b753721460
Andrey Ikonnikov <avikonnikov@edu.hse.ru>
2023-10-21 15:00:24 +0300

    SimpleCommentsOnHeadbob

--- Assets/Scripts/HeadBob_Controller.cs
+++ Assets/Scripts/HeadBob_Controller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Headbob : MonoBehaviour
 {
-    [Header("Configuration")]
+    [Header("Configuration")] // Headbob settings menu
     [SerializeField] private bool _enable = true;
     [SerializeField, Range(0, 0.1f)] private float _Amplitude = 0.015f; 
     [SerializeField, Range(0, 30)] private float _frequency = 10.0f;
@@ -27,7 +27,7 @@ public class Headbob : MonoBehaviour
         ResetPosition();
         _camera.LookAt(FocusTarget());
     }
-    private Vector3 FootStepMotion()
+    private Vector3 FootStepMotion() // some funny physics stuff
     {
         Vector3 pos = Vector3.zero;
         pos.y += Mathf.Sin(Time.time * _frequency) * _Amplitude;
@@ -47,13 +47,13 @@ public class Headbob : MonoBehaviour
         _camera.localPosition += motion;
     }
 
-    private Vector3 FocusTarget()
+    private Vector3 FocusTarget() // focus code
     {
         Vector3 pos = new Vector3(transform.position.x, transform.position.y + _cameraHolder.localPosition.y, transform.position.z);
         pos += _cameraHolder.forward * 15.0f;
         return pos;
     }
-    private void ResetPosition()
+    private void ResetPosition() // reset code
     {
         if (_camera.localPosition == _startPos) return;
         _camera.localPosition = Vector3.Lerp(_camera.localPosition, _startPos, 1 * Time.deltaTime);
